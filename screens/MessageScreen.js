1c1a23c5e2a681da523ff1ff08c16ecb177d57a7
Sanjaykumar <sanjaykumar24official@gmail.com>
2024-03-21 11:31:47 +0530

    added socket

--- screens/MessageScreen.js
+++ screens/MessageScreen.js
@@ -1,21 +1,37 @@
 import React, { useContext, useEffect, useState } from 'react'
-//import { io } from 'socket.io-client';
+import { io } from 'socket.io-client';
 import {View,Text, Touchable, TouchableOpacity} from 'react-native'
 import { Context } from '../context/Context'
 import { StatusBar } from 'expo-status-bar'
 import { FlatList,TextInput } from 'react-native-gesture-handler'
 export default function MessageScreen()
 {
-  //const socket = io('http://localhost:5000')
+  const socket = io('http://172.16.123.22:5000')
   const {groupId,setMessage,message,username} = useContext(Context)
   const [data,setData] = useState([])
-  // useEffect(()=>{
-  //   socket.on('data',(data)=>{
-  //     setData([data,...data]);
-  //   })
-  // },[socket])
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('Connected to server');
+      socket.emit('joinRoom', { room: groupId });
+    });
+  
+    const handleData = (newData) => {
+      console.log('Received new data:', newData);
+      setData(prevData => [newData, ...prevData]);
+    };
+  
+    socket.on('data', handleData);
+  
+    return () => {
+      socket.off('data', handleData);
+    }
+  }, [socket]);
+  
+
+  
   const addmessage = () =>{
-    //socket.emit('message',{name:username,message,message})
+    socket.emit('message',{name:username,message:message,room:groupId})
     setMessage('')
   }
   return (
@@ -25,29 +41,31 @@ export default function MessageScreen()
     <Text style={{fontSize:30}}>chats</Text>
     </View>
     <View style={{width:'100%',height:'85%'}}>
-    <FlatList data={data} inverted renderItem={(i)=>{
-      let position = 'flex-start'
-      let name = i.item.name
-      let left = '5%'
-      let right = '0%'
-      if(i.item.name == username)
-      {
-        position = 'flex-end'
-        name = 'you'
-        left = '0%'
-        right = '5%'
+    <FlatList inverted
+    data={data}
+    renderItem={({ item }) => {
+      let position = 'flex-start';
+      let name = item.name;
+      let left = '5%';
+      let right = '0%';
+      if (item.name === username) {
+        position = 'flex-end';
+        name = 'you';
+        left = '0%';
+        right = '5%';
       }
-       return (
-        <View style={{width:'100%',height:'auto'}}>
-        <View style={{flexDirection:'row',width:'100%',height:'auto',justifyContent:position,alignItems:'center',marginTop:'8%'}}> 
-        <View style={{marginRight:right,marginLeft:left,width:'50%',flexDirection:'row',justifyContent:position}}>
-        <Text style={{color:'red'}}>{name} : </Text>
-        <Text>{i.item.message}</Text>
+      return (
+        <View style={{ width: '100%', height: 'auto' }}>
+          <View style={{ flexDirection: 'row', width: '100%', height: 'auto', justifyContent: position, alignItems: 'center', marginTop: '8%' }}>
+            <View style={{ marginRight: right, marginLeft: left, width: '50%', flexDirection: 'row', justifyContent: position }}>
+              <Text style={{ color: 'red' }}>{name} : </Text>
+              <Text>{item.message}</Text>
+            </View>
+          </View>
         </View>
-        </View>
-        </View>
-       )
-    }}/>
+      );
+    }}
+  />  
     </View>
     <View style={{width:'100%',height:'25%',flexDirection:'row',justifyContent:'center'}}>
     <View style={{width:'95%',height:'75%',flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
